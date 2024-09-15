f47f676c91eafcd0090e2abc029b39586b64bf52
MuliJob <munyoki912@gmail.com>
2024-09-15 14:55:22 +0300

    adding search form

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.news_today,name = 'newsToday'),
-    path(r'archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews')
+    path(r'archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews'),
+    path(r'search/', views.search_results,name = 'search_results')
 ]
