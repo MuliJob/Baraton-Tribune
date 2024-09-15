8fc6cb1085ed84dec4ea38e6188dd86e4f415101
MuliJob <munyoki912@gmail.com>
2024-09-15 13:51:46 +0300

    updating views

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.news_of_day,name = 'newsToday'),
+    path('',views.news_today,name = 'newsToday'),
     path(r'archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews')
 ]
