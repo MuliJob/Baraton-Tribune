395060ac28fb24f98e40ced4d03a6cc12309cc45
MuliJob <munyoki912@gmail.com>
2024-09-16 11:54:15 +0300

    initial commit

--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 import datetime as dt
-from .models import Article
+from .models import Article, NewsLetterRecipients
 from .forms import NewsLetterForm
 
 # Create your views here.
