09378ecb9ab943e3441bec2d803c355935ec0d52
MuliJob <munyoki912@gmail.com>
2024-09-19 17:08:32 +0300

    added logout url

--- tribune/urls.py
+++ tribune/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path,include
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('news.urls')),
     path('accounts/',include('registration.backends.simple.urls')),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
