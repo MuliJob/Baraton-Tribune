ca33bdcad7d67e548f35eeb4f15ef32edd4013fb
MuliJob <munyoki912@gmail.com>
2024-09-19 16:56:49 +0300

    django authentication, registration

--- tribune/settings.py
+++ tribune/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'news',
     'bootstrap3',
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
