16b962fac38cbe1a77194561146de544a465edb5
MuliJob <munyoki912@gmail.com>
2024-10-06 17:00:17 +0300

    handling post requests

--- news/views.py
+++ news/views.py
@@ -16,6 +16,12 @@ class MerchList(APIView):
         all_merch = BaratonMerch.objects.all()
         serializers = MerchSerializer(all_merch, many=True)
         return Response(serializers.data)
+    def post(self, request, format=None):
+        serializers = MerchSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def news_today(request):
     date = dt.date.today()
