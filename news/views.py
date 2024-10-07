8b0c920147ee46fe6a1bb875c8acb6f28b6d2511
MuliJob <munyoki912@gmail.com>
2024-10-07 13:06:37 +0300

    deleting an object

--- news/views.py
+++ news/views.py
@@ -46,6 +46,11 @@ class MerchDescription(APIView):
             return Response(serializers.data)
         else:
             return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        merch.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 def news_today(request):
     date = dt.date.today()
